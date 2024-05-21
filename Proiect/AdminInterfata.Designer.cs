ea02a2a76516a21d1d2cb4fa3db6ca7fcd044ad0
Unicatm <alexandra.tertiscu@gmail.com>
2024-05-22 00:22:08 +0300

    interfata admind design

---
+++ Proiect/AdminInterfata.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Proiect
+{
+    partial class AdminInterfata
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.salvareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restaurareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xMLToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.LbAdminEmail = new System.Windows.Forms.Label();
+            this.filmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adaugareToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(36, 131);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1300, 700);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.SandyBrown;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salvareToolStripMenuItem,
+            this.restaurareToolStripMenuItem,
+            this.filmToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1382, 51);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // salvareToolStripMenuItem
+            // 
+            this.salvareToolStripMenuItem.BackColor = System.Drawing.Color.SandyBrown;
+            this.salvareToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.xMLToolStripMenuItem});
+            this.salvareToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.salvareToolStripMenuItem.Name = "salvareToolStripMenuItem";
+            this.salvareToolStripMenuItem.Padding = new System.Windows.Forms.Padding(25, 20, 25, 0);
+            this.salvareToolStripMenuItem.Size = new System.Drawing.Size(118, 47);
+            this.salvareToolStripMenuItem.Text = "Salvare";
+            // 
+            // xMLToolStripMenuItem
+            // 
+            this.xMLToolStripMenuItem.Name = "xMLToolStripMenuItem";
+            this.xMLToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.xMLToolStripMenuItem.Text = "XML";
+            // 
+            // restaurareToolStripMenuItem
+            // 
+            this.restaurareToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.xMLToolStripMenuItem1});
+            this.restaurareToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.restaurareToolStripMenuItem.Name = "restaurareToolStripMenuItem";
+            this.restaurareToolStripMenuItem.Padding = new System.Windows.Forms.Padding(25, 0, 25, 0);
+            this.restaurareToolStripMenuItem.Size = new System.Drawing.Size(145, 47);
+            this.restaurareToolStripMenuItem.Text = "Restaurare";
+            // 
+            // xMLToolStripMenuItem1
+            // 
+            this.xMLToolStripMenuItem1.Name = "xMLToolStripMenuItem1";
+            this.xMLToolStripMenuItem1.Size = new System.Drawing.Size(224, 28);
+            this.xMLToolStripMenuItem1.Text = "XML";
+            // 
+            // LbAdminEmail
+            // 
+            this.LbAdminEmail.AutoSize = true;
+            this.LbAdminEmail.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LbAdminEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbAdminEmail.Location = new System.Drawing.Point(1070, 9);
+            this.LbAdminEmail.Name = "LbAdminEmail";
+            this.LbAdminEmail.Size = new System.Drawing.Size(173, 25);
+            this.LbAdminEmail.TabIndex = 2;
+            this.LbAdminEmail.Text = "admin@gmial.com";
+            // 
+            // filmToolStripMenuItem
+            // 
+            this.filmToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.adaugareToolStripMenuItem});
+            this.filmToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.filmToolStripMenuItem.Name = "filmToolStripMenuItem";
+            this.filmToolStripMenuItem.Padding = new System.Windows.Forms.Padding(25, 0, 25, 0);
+            this.filmToolStripMenuItem.Size = new System.Drawing.Size(95, 47);
+            this.filmToolStripMenuItem.Text = "Film";
+            // 
+            // adaugareToolStripMenuItem
+            // 
+            this.adaugareToolStripMenuItem.Name = "adaugareToolStripMenuItem";
+            this.adaugareToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.adaugareToolStripMenuItem.Text = "Adaugare";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(36, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // AdminInterfata
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(1382, 853);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LbAdminEmail);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "AdminInterfata";
+            this.Text = "AdminInterfata";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem salvareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restaurareToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xMLToolStripMenuItem1;
+        private System.Windows.Forms.Label LbAdminEmail;
+        private System.Windows.Forms.ToolStripMenuItem filmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adaugareToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
